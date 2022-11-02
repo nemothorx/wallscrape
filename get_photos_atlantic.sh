a99873142868eb6081aadd56d3011da5d659b18d
Nemo Thorx <nemo@thorx.net>
2022-11-03 00:59:52 +1000

    get_potw_atlantic renamed to get_photos_atlantic  - logging functions and other post-rewrite polish
    renamed:   ...atlantic.sh => get_photos_atlantic.sh  | 108 +++++++++++----
     1 file changed, 80 insertions(+), 28 deletions(-)

--- get_photos_atlantic.sh
+++ get_photos_atlantic.sh
@@ -1,37 +1,68 @@
 #!/bin/bash
 
-LOGFILE=$HOME/var/log/potw_atlantic.log
+mainlog=$HOME/var/log/photos_atlantic.log
 YEAR=$1
 MONTH=$2
 
-basedir=/srv/Images/AtlanticPOTW
+basedir=/srv/Images/AtlanticPhotos
 
-LOGTAG=""           # tag each log line for a given message with this. 
 
-# if these not set, we should assume the current month... 
+LOGTAG=" "           # tag each log line for a given message with this.
+do_log() {
+    # if we're fed params, then echo that into ourselves
+    if [ -n "$1" ] ; then
+        echo $@ | do_log
+    else
+        # no param so assume stdin.
+        # This logs per-line the output of other programs with multiline output
+        cat /dev/stdin | while read line ; do
+            if [ -t 1 ] ; then
+                echo "$(date +"%FT%T")${LOGTAG}${line}" | tee -a $mainlog
+            else
+                echo "$(date +"%FT%T")${LOGTAG}${line}" >> $mainlog
+            fi
+        done
+    fi
+}
+
+do_logcmd() {
+    # logcmd logs a command to be run, then runs it
+    # note: if command has a password, it could be __REDACTED__ here if known
+    # eg, by piping through this before dolog: sed -e "s/$pw/__REDACTED__/g" 
+    echo "$ $@" | do_log
+    echo "$@" | bash 2>&1 | do_log
+    #TODO: would be neat if this could detect non-zero exit and log to suit. 
+    # ...but I think the pipe into do_log eats that. Test and solve?
+}
+
+
+# if these not set, we should assume the current month 
 
 [ -z $YEAR ] && YEAR=$(date +%Y)
 [ -z $MONTH ] && MONTH=$(date +%m)
 
 
-echo "Retrieving weekly photos from the atlantic for $YEAR/$MONTH"
+do_log "# Retrieving photos from the atlantic for $YEAR/$MONTH"
 
 baseurl="https://www.theatlantic.com"
 
 monthurl="${baseurl}/photo/$YEAR/$MONTH/"
-echo ": $monthurl"
+do_log ": $monthurl"
 allinmonth=$(curl -S -s "$monthurl" | grep "a data-omni-click=.inherit. href=./photo/$YEAR/$MONTH/" | cut -d'"' -f 4)
 
 for link in $allinmonth ; do
     groupurl="${baseurl}${link}"
-    echo ": $groupurl"
+    do_log ": Processing $groupurl"
     groupdir=$basedir/$YEAR/$MONTH/$(echo "$groupurl" | cut -d/ -f 7)
-    echo "> $groupdir"
-    mkdir -p "$groupdir"
+#    do_log "+ creating: $groupdir"
+    do_logcmd "mkdir -pv \"$groupdir\""
     cd "$groupdir"
 
-    # get the URL and process it into markdown
-    # ...this file will be mostly the final version, but we'll do a second round of processing to obtain the actual images
+    # get the URL and process it into markdown - which will be mostly the final
+    # version but will have ALL image URLs within it. 
+    #
+    # We then do a second round of processing to obtain actual images and
+    # finalise the markdown
     curl -S -s "$groupurl" | awk -v URL="$groupurl" -v firstimg="waiting" -F'"' '
 
 # Obtain the html title
@@ -115,25 +146,41 @@ END {
     print ""
     print "- Processed from: "URL
 }
-    ' > tmp.md
+    ' | sed -e 's/\xC2\xA0/ /g' > tmp.md
+    # above sed converts nonbreaking whitespace into regular space
+    # ...it otherwise screws up `recode html..utf8` (even though that's been replaced by python)
 
     # the filename we want for our final markdown is going to be the directory the images are in, and we can now calculate that
     # original URLs are all in a path that includes "thumbor" so we're keying from that
     mdfile=$(cat tmp.md | grep thumbor | cut -d/ -f 12 | head -1).md
-    echo ": Processing tmp to make $mdfile"
+    do_log ": Processing tmp.md into $mdfile +image downloads"
 
-    cat tmp.md | while read line ; do 
+#    cat tmp.md | recode html..utf8 | while read line ; do 
+#    ...recode was catching too much stuff and causing errors. 
+#       eg: turning  "Australia’s" into "Australiaâ  s"
+#       see: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=748984 
+#       workaround that works: | recode -d utf8..html | recode html..utf8
+#    ...but this python does the right thing and isn't working around bugs. so:
+    cat tmp.md | python3 -c 'import html,sys; print(html.unescape(sys.stdin.read()), end="")' | while read line ; do 
         case $line in
             *thumbor*)
                 imgurl=$line
                 imgtype=${imgurl##*.}
                 imgname=$(echo "$imgurl" | cut -d/ -f 13)
                 imgfinal="$imgname.$imgtype"
-                echo "> $imgurl -> $imgfinal"
+                do_log "> $imgurl"
                 # these have no accurate mtime from http headers, so -O is OK
-                wget -nv "$imgurl" -O $imgfinal
+                do_logcmd "wget -nc -nv \"$imgurl\" -O $imgfinal"
+
+                # note: this identify will exit1 if there are errors
+                # ...however, I don't know if that exitcode travels back all the way to here
+                # TODO: find that out. retry/log/whatever if it does
+                do_log ": $(identify -regard-warnings $imgfinal)"
+                
+                # this is what embedded images in markdown look like 
+                # note: no alt text since we've turned that into the header
+                # for each block
                 echo "!($imgfinal)" >> $mdfile
-                sleep 1
                 ;;
             *)
                 echo "$line" >> $mdfile
@@ -142,17 +189,23 @@ END {
     done
 
     wc -l $mdfile
-    rm tmp.md
+    do_logcmd "ls -l $mdfile"
+    # TODO: sanity checks:
+    # * image download count vs images mentioned in markdown
+    # * images are complete (imagemagick has some verify option)
+    # rm tmp.md
 
-    sleep 5
-
-    # TODO: parse that mid-stage markdown for images-to-download and create final markdown
-    # ...images embedded or linked?
+    sleep $((RANDOM%5+3)) # sleep for 3/4/5/6/7 seconds before looping to the next gallery
+#    sleep $((RANDOM%3+1)) # sleep for 2/3/4 seconds before looping
 
 done
 
-exit 5
+# TODO: some post-analysis of what has been gotten
+# ...for logs and also maybe for send2pushover ?
 
+
+
+# notes about older images
 
 
 # notes from 2020-05:
@@ -164,13 +217,12 @@ exit 5
 
    
 
-    # logging... grab from the get_potd.log stuff. 
-    # ...perhaps fold this into that??
-
-
 #
 # can find older photo of the week pages within YYYY/MM pages starting here:
 # https://www.theatlantic.com/photo/2011/02/
 # ...noting that that's ALL photo pages, POTW specific started in 2014/05
+
+
+
 
 
